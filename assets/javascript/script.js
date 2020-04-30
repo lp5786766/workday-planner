23878fbebe6f3ebc7ffa9772806b257a35db63a4
Luba Pecheneva <lp5786766@gmail.com>
2020-04-29 21:52:00 -0400

    Added pseudocode

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,24 @@
-alert("Hello!");
+// When I open the page, today's date is displayed (Moment.js library).
+// Below I see 9 blocks of 1 hour each (9:00 am - 5:00 pm);
+    // This should be covered in the HTML file.
+        // time---task-field---task---saveBtn
+// Each block has functionality:
+    // 1. I can click on the block the record my task. 
+    // After I input text, I click the save button to the side of the block and the task persits.
+    // *****************************************************SAVE BUTTON*************************************************************
+    // saveBtn on.("click"). How to distinguish between buttons?
+
+    // The text is stored in the local storage.
+    // input="text";
+    // The text is taken from the local storage for display.
+    // Text could be deleted from the local storage.
+    // 
+    // 2. I can click on the existing task and erase it, I can write a new task on the same spot and save it afterwards.
+    // 3. Each block is colored respective of time (past, current, future event);
+
+
+// Questions/Suggestions:
+    // Would the tasks be deleted the next day and you are presented with a blank day?
+    // Mayby add button "Clear all Tasks"
+    // Maybe add options to highlight events or timeblocks.
+    // 
