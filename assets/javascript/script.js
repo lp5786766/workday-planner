624afe34cb39828805fe78dbad70790dc86df885
Luba Pecheneva <lp5786766@gmail.com>
2020-05-02 18:52:46 -0400

    Added current event state

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,37 +1,36 @@
 // When I open the page, today's date is displayed (Moment.js library).
 
-var moment = moment().format("MMMM Do YYYY");
-$("#moment").text(moment);
+var momentDay = moment().format("MMMM Do YYYY");
+$("#moment").text(momentDay);
 
 // Below I see 9 blocks of 1 hour each (9:00 am - 5:00 pm);
 // Each block has functionality:
-// 1. I can click on the block the record my task.
+// I can click on the block the record my task.
 // After I input text, I click the Save button to the side of the block and the task persits.
 
 // *****************************************************SAVE BUTTON*************************************************************
-// saveBtn on.("click"). How to distinguish between buttons?
 var saveBtn = $(".save");
+var task9 = $(".9");
+var task10 = $(".10");
+var task11 = $(".11");
+var task12 = $(".12");
+var task1 = $(".1");
+var task2 = $(".2");
+var task3 = $(".3");
+var task4 = $(".4");
+var task5 = $(".5");
 
 saveBtn.on("click", function () {
   // The text is stored in the local storage.
-  var task9 = $(".9").val();
-  localStorage.setItem("task9", task9);
-  var task10 = $(".10").val();
-  localStorage.setItem("task10", task10);
-  var task11 = $(".11").val();
-  localStorage.setItem("task11", task11);
-  var task12 = $(".12").val();
-  localStorage.setItem("task12", task12);
-  var task1 = $(".1").val();
-  localStorage.setItem("task1", task1);
-  var task2 = $(".2").val();
-  localStorage.setItem("task2", task2);
-  var task3 = $(".3").val();
-  localStorage.setItem("task3", task3);
-  var task4 = $(".4").val();
-  localStorage.setItem("task4", task4);
-  var task5 = $(".5").val();
-  localStorage.setItem("task5", task5);
+  localStorage.setItem("task9", task9.val());
+  localStorage.setItem("task10", task10.val());
+  localStorage.setItem("task11", task11.val());
+  localStorage.setItem("task12", task12.val());
+  localStorage.setItem("task1", task1.val());
+  localStorage.setItem("task2", task2.val());
+  localStorage.setItem("task3", task3.val());
+  localStorage.setItem("task4", task4.val());
+  localStorage.setItem("task5", task5.val());
 });
 // The text is taken from the local storage for display.
 
@@ -53,11 +52,44 @@ var savedTask4 = localStorage.getItem("task4");
 $(".4").val(savedTask4);
 var savedTask5 = localStorage.getItem("task5");
 $(".5").val(savedTask5);
-// Text could be deleted from the local storage.
+
+// I can click on the existing task and erase it, I can write a new task on the same spot and save it afterwards.
+
+// 3. Each block is colored respective of time (past, current, future event);
+
+// var momentTime = moment().format("h");
+// console.log(momentTime);
+var momentTime = 11;
+console.log(momentTime);
+var allTasks = [
+  task9,
+  task10,
+  task11,
+  task12,
+  task1,
+  task2,
+  task3,
+  task4,
+  task5,
+];
+
+allTasks.forEach(function (task) {
+  var taskID = parseInt(task[0].id);
+  console.log(taskID);
+  if (taskID === momentTime) {
+    task.css("background-color", "rgb(255, 70, 70)");
+  }
+});
+
 //
 
-// 2. I can click on the existing task and erase it, I can write a new task on the same spot and save it afterwards.
-// 3. Each block is colored respective of time (past, current, future event);
+// var element = taskEl;
+// var idStr = element.id;
+// var taskTime = $("input").id;
+// var taskId = taskTime.id;
+
+// if
+// if the index is less than time then color...
 
 // Questions/Suggestions:
 // Would the tasks be deleted the next day and you are presented with a blank day?
